cfa3c1d090ee4bf292eb36847352adc879a23285
Хрімлі Віктор Вікторович <viktorhrimli101@gmail.com>
2022-10-31 23:05:13 +0200

    main

---
+++ Index.js
@@ -0,0 +1,12 @@
+const {
+  addContact,
+  getContactById,
+  listContacts,
+  removeContact,
+} = require("./contacts");
+
+// listContacts();
+// getContactById("5");
+addContact("13", "Viktr", "mango@gmail.com", "+38053533432");
+
+// removeContact("9");
